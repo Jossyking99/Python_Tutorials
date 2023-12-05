12780965e9450f66cc44827f23e73e1063535d1b
Jossyking99 <Josephakharume97@gmail.com>
2023-12-05 01:52:57 +0000

    docstring files added

---
+++ docstring.py
@@ -0,0 +1,13 @@
+def calculate_square(number):
+    """Calculates the square of a given number.
+
+    Parameter:
+    number(int or float): The number to be squared.
+
+    Return:
+    int or float:The square of the input number.
+    """
+    print(number**2)
+
+calculate_square(7)
+print(calculate_square. __doc__)
---
+++ docstring_2.py
@@ -0,0 +1,5 @@
+def square(n):
+    '''Takes in a number n, and return the square of n'''
+    print(n**2)
+square(9)
+print(square.__doc__)
